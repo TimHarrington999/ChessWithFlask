7e091303f73c3ab57ceda37cec6d58114139a1e9
TimHarrington999 <tharrington999@gmail.com>
2026-04-09 18:55:43 -0500

    progress on chess engine

--- engine/main.c
+++ engine/main.c
@@ -8,40 +8,19 @@
 // ## Overview ##
 // This connects with Flask through the python class UCIEngine via pipe.
 // Communication is first established through the handle_uci() function like a
-// handshake. Flask will tell us the board position and then how far to search
+// handshake.
+// Flask will tell us the board position and then how far to search
 // for a move.
 //
 
 #include "main.h"
+#include <stdlib.h>
 
 #define MAX_LINE 1024
 
-typedef enum { WHITE = 0, BLACK = 1 } color;
-
 bool running = true;
 
-typedef struct {
-  union {
-    uint64_t wp;
-    uint64_t wr;
-    uint64_t wn;
-    uint64_t wb;
-    uint64_t wq;
-    uint64_t wk;
-
-    uint64_t bp;
-    uint64_t br;
-    uint64_t bn;
-    uint64_t bb;
-    uint64_t bq;
-    uint64_t bk;
-  } board;
-
-  color turn;
-
-} Game;
-
-Game *game;
+Game *game; // global for simplicity
 
 //
 // #################
@@ -62,9 +41,9 @@ void handle_isready() {
 }
 
 // There are three possibilities when handling position
-// 1. 'startpos'
-// 2. 'startpos moves e2e4 e7e5'
-// 3. 'fen <FEN>'
+// 1. (Just the start position) - 'startpos'
+// 2. (start position and a move sequence) - 'startpos moves e2e4 e7e5'
+// 3. (FEN string) - 'fen <FEN>'
 void handle_position() {
   return;
 
@@ -82,12 +61,12 @@ void handle_position() {
       while (fgets(move, sizeof(move), stdin)) {
         move[strcspn(move, "\n")] = 0;
 
-        // board_move_piece(move);
+        board_move_piece(move);
       }
     } else if (strcmp(word, "fen") == 0) {
       char fen[MAX_LINE];
 
-      // parse fen
+      board_load_fen(fen);
     }
   }
 }
@@ -108,7 +87,25 @@ void handle_quit() { running = false; }
 // ##################
 //
 
-void board_load_startpos() {}
+void board_load_startpos() {
+  game->board->wp = 0x000000000000FF00;
+  game->board->wr = 0x0000000000000081;
+  game->board->wn = 0x0000000000000042;
+  game->board->wb = 0x0000000000000024;
+  game->board->wq = 0x0000000000000010;
+  game->board->wk = 0x0000000000000008;
+
+  game->board->bp = 0x00FF000000000000;
+  game->board->br = 0x8100000000000000;
+  game->board->bn = 0x4200000000000000;
+  game->board->bb = 0x2400000000000000;
+  game->board->bq = 0x1000000000000000;
+  game->board->bk = 0x0800000000000000;
+}
+
+void board_move_piece(char *move) {}
+
+void board_load_fen(char *fen) {}
 
 //
 // ####################
@@ -116,10 +113,42 @@ void board_load_startpos() {}
 // ####################
 //
 
+// The following shows how the square variable is indexed according to the board
+
+// Bit index:   63 ................. 0
+//
+// Board:
+// 8  [56 57 58 59 60 61 62 63]
+// 7  [48 49 50 51 52 53 54 55]
+// 6  [40 41 42 43 44 45 46 47]
+// 5  [32 33 34 35 36 37 38 39]
+// 4  [24 25 26 27 28 29 30 31]
+// 3  [16 17 18 19 20 21 22 23]
+// 2  [ 8  9 10 11 12 13 14 15]
+// 1  [ 0  1  2  3  4  5  6  7]
+//
+//      a  b  c  d  e  f  g  h
+
+void set_bit(uint64_t *board, int square) { *board |= (1ULL << square); }
+
+void clear_bit(uint64_t *board, int square) { *board &= ~(1ULL << square); }
+
+void move_bit(uint64_t *board, int from, int to) {
+  clear_bit(board, from);
+  set_bit(board, to);
+}
+
+// ########################################
+
 int main() {
 
   game = malloc(sizeof(Game));
   if (game == NULL) {
+    return 1;
+  }
+
+  game->board = malloc(sizeof(Board));
+  if (game->board == NULL) {
     return 1;
   }
 
--- engine/main.h
+++ engine/main.h
@@ -4,6 +4,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum { WHITE = 0, BLACK = 1 } color;
+
+typedef struct {
+  uint64_t wp;
+  uint64_t wr;
+  uint64_t wn;
+  uint64_t wb;
+  uint64_t wq;
+  uint64_t wk;
+
+  uint64_t bp;
+  uint64_t br;
+  uint64_t bn;
+  uint64_t bb;
+  uint64_t bq;
+  uint64_t bk;
+} Board;
+
+typedef struct {
+  Board *board;
+  color turn;
+
+} Game;
+
 // UCI Functions
 void handle_uci();
 void handle_isready();
@@ -13,5 +37,10 @@ void handle_quit();
 
 // Main Functions
 void board_load_startpos();
+void board_move_piece(char *);
+void board_load_fen(char *);
 
 // Bitmap Functions
+void set_bit(uint64_t *, int);
+void clear_bit(uint64_t *, int);
+void move_bit(uint64_t *, int, int);
