1ee5ef2f24d1624b3b521cf984133e2031a6839c
TimHarrington999 <tharrington999@gmail.com>
2026-07-05 18:05:52 -0700

    fix path

--- frontend/app.py
+++ frontend/app.py
@@ -4,10 +4,10 @@ from flask import Blueprint, Flask, jsonify, render_template, request
 from engine import UCIEngine
 
 # This path defines where the executable for the engine implementation is
-# The c engine is engine/engine
-# The c++ engine is engine/enginecpp
-# engine = UCIEngine("engine/engine")
-engine = UCIEngine("engine/enginecpp")
+# The c engine is engine_c/engine
+# The c++ engine is engine_cpp/engine
+# engine = UCIEngine("engine_c/engine")
+engine = UCIEngine("engine_cpp/engine")
 
 # TODO: Move game list to a database instead of being stored in memory
 the_game = chess.Board()
